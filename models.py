86a4a027953fb12c26e93f54cfd8f939d2c8b3a9
Francesco Bottalico <francesco2031@outlook.it>
2024-08-29 12:37:00 +0200

    Add kwargs to models

--- models.py
+++ models.py
@@ -1,11 +1,10 @@
 import torch.nn as nn
 
 class ModelLSTM(nn.Module):
-    def __init__(self, input_size, output_size, num_layers=1, bidirectional=False):
+    def __init__(self, input_size, output_size, **kwargs):
         super(ModelLSTM, self).__init__()
         self.hidden_size = (input_size + output_size) // 2
-        self.num_layers = num_layers
-        self.lstm = nn.LSTM(self.hidden_size, self.hidden_size, num_layers, batch_first=True, bidirectional=bidirectional)
+        self.lstm = nn.LSTM(self.hidden_size, self.hidden_size, batch_first=True, **kwargs)
         self.relu = nn.ReLU()
         self.fc1 = nn.Linear(input_size, self.hidden_size)
         self.fc2 = nn.Linear(self.hidden_size, output_size)
@@ -17,11 +16,10 @@ class ModelLSTM(nn.Module):
         return out
 
 class ModelGRU(nn.Module):
-    def __init__(self, input_size, output_size, num_layers=1):
+    def __init__(self, input_size, output_size, **kwargs):
         super(ModelGRU, self).__init__()
         self.hidden_size = (input_size + output_size) // 2
-        self.num_layers = num_layers
-        self.gru = nn.GRU(self.hidden_size, self.hidden_size, num_layers, batch_first=True)
+        self.gru = nn.GRU(self.hidden_size, self.hidden_size, batch_first=True, **kwargs)
         self.relu = nn.ReLU()
         self.fc1 = nn.Linear(input_size, self.hidden_size)
         self.fc2 = nn.Linear(self.hidden_size, output_size)
